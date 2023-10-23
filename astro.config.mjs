9761cc742732a642d0ef29c65b648841663691e6
melaveetha <retromeras@ya.ru>
2023-10-23 21:12:05 +0300

    Add [site and base] astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import purgecss from "astro-purgecss";
 // https://astro.build/config
 export default defineConfig({
   integrations: [purgecss()],
+  site: "https://blue-crane.github.io",
+  base: "/accept_landing"
 });
